c53bff95676dab329e948904118e8c5ff989542c
freakcoderz <42624259+freakcoderz@users.noreply.github.com>
2020-07-10 16:06:45 -0400

    Update helpTipper.js

--- bot/modules/helpTipper.js
+++ bot/modules/helpTipper.js
@@ -1,17 +1,6 @@
 'use strict';
 let config = require('config');
-let ravenFee = config.get('rvn').paytxfee;
-let dogeFee = config.get('doge').paytxfee;
-let lbryFee = config.get('lbc').paytxfee;
-let phaseFee = config.get('phase').paytxfee;
-let ufoFee = config.get('ufo').paytxfee;
-let phoenixFee = config.get('pxc').paytxfee;
-let featherFee = config.get('ftc').paytxfee;
-let verticalFee = config.get('vtl').paytxfee;
-let neblioFee = config.get('nebl').paytxfee;
-let xuezFee = config.get('xuez').paytxfee;
-let spkFee = config.get('spk').paytxfee;
-let ppcFee = config.get('ppc').paytxfee;
+let ravenFee = config.get('stone').paytxfee;
 let prefix = config.get('bot').prefix;
 exports.commands = ['tiphelp'];
 exports.tiphelp = {
@@ -20,18 +9,7 @@ exports.tiphelp = {
   process: function(bot, message) {
     message.author.send(
       '__**:bank: Coins :bank:**__\n' +
-      '  **Ravencoin (RVN) Tipper**\n    Transaction Fees: **' + ravenFee + '**\n' +
-      '  **Dogecoin (DOGE) Tipper**\n    Transaction Fees: **' + dogeFee + '**\n' +
-      '  **LBRY Credit (LBC) Tipper**\n    Transaction Fees: **' + lbryFee + '**\n' +
-      '  **Phase (PHASE) Tipper**\n    Transaction Fees: **' + phaseFee + '**\n' +
-      '  **Uniform Fiscal Object (UFO) Tipper**\n    Transaction Fees: **' + ufoFee + '**\n' +
-      '  **Phoenixcoin (PXC) Tipper**\n    Transaction Fees: **' + phoenixFee + '**\n' +
-      '  **Feathercoin (FTC) Tipper**\n    Transaction Fees: **' + featherFee + '**\n' +
-      '  **Vertical (VTL) Tipper**\n    Transaction Fees: **' + verticalFee + '**\n' +
-      '  **Neblio (NEBL) Tipper**\n    Transaction Fees: **' + neblioFee + '**\n' +
-      '  **Xuez (XUEZ) Tipper**\n    Transaction Fees: **' + xuezFee + '**\n' +
-      '  **SparksPay (SPK) Tipper**\n    Transaction Fees: **' + spkFee + '**\n' +      
-      '  **Peercoin (PPC) Tipper**\n    Transaction Fees: **' + ppcFee + '**\n' +         
+      '  **Stonecoin (STONE) Tipper**\n    Transaction Fees: **' + stoneFee + '**\n' +    
       '__**Commands**__\n' +
       '  **' + prefix + 'tip<CoinSymbol>** : Displays This Message\n' +
       '  **' + prefix + 'tip<CoinSymbol> balance** : get your balance\n' +
@@ -41,11 +19,11 @@ exports.tiphelp = {
       '  **' + prefix + 'tip<CoinSymbol> private <user> <amount>** : put private before Mentioning a user to tip them privately\n' +
       '**<> : Replace carrot <> symbole with appropriate value.**\n' +
       '__**Examples**__\n' +
-      '  **' + prefix + 'tiprvn @MSFTserver 10**\n' +
-      '  **' + prefix + 'tipdoge withdraw DOGEaddressHERE 10**\n' +
-      '  **' + prefix + 'tipftc private @MSFTserver 10**\n' +
-      '  **' + prefix + 'tiplbc balance**\n' +
-      '  **' + prefix + 'tippxc deposit**\n'
+      '  **' + prefix + 'tipstone @FreakHouse 10**\n' +
+      '  **' + prefix + 'tipstone withdraw STONEaddressHERE 10**\n' +
+      '  **' + prefix + 'tipstone private @FreakHouse 10**\n' +
+      '  **' + prefix + 'tipstone balance**\n' +
+      '  **' + prefix + 'tipstone deposit**\n'
     );
   }
 };
