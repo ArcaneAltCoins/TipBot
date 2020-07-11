9618b31c8f5ca8428a27153805e1e10a237b420d
freakcoderz <42624259+freakcoderz@users.noreply.github.com>
2020-07-11 19:34:21 -0400

    Create aevo.js

---
+++ bot/modules/aevo.js
@@ -0,0 +1,444 @@
+'use strict';
+
+const bitcoin = require('bitcoin');
+const Discord = require('discord.js');
+
+let Regex = require('regex'),
+  config = require('config'),
+  spamchannels = config.get('moderation').botspamchannels;
+let walletConfig = config.get('aevo').config;
+let paytxfee = config.get('aevo').paytxfee;
+const aevo = new bitcoin.Client(walletConfig);
+const XMLHttpRequest = require('xmlhttprequest').XMLHttpRequest;
+
+exports.commands = ['aevo'];
+exports.aevo = {
+  usage: '<subcommand>',
+  description:
+    'AevoCoin',
+  process: async function(bot, msg, suffix) {
+    let tipper = msg.author.id.replace('!', ''),
+      words = msg.content
+        .trim()
+        .split(' ')
+        .filter(function(n) {
+          return n !== '';
+        }),
+      subcommand = words.length >= 2 ? words[1] : 'help',
+      helpmsg =
+        'Please type !help for a DM of commands and supported coins.',
+      channelwarning = 'Please use <#bot-spam> or DMs to talk to bots.';
+    switch (subcommand) {
+      case 'help':
+        privateorSpamChannel(msg, channelwarning, doHelp, [helpmsg]);
+        break;
+      case 'balance':
+        doBalance(msg, tipper);
+        break;
+      case 'deposit':
+        privateorSpamChannel(msg, channelwarning, doDeposit, [tipper]);
+        break;
+      case 'withdraw':
+        privateorSpamChannel(msg, channelwarning, doWithdraw, [tipper, words, helpmsg]);
+        break;
+      case 'block':
+        getBlock(bot, msg);
+        break;
+      case 'supply':
+        getSupply(bot, msg);
+        break;
+      default:
+        doTip(bot, msg, tipper, words, helpmsg);
+    }
+  }
+};
+
+function privateorSpamChannel(message, wrongchannelmsg, fn, args) {
+  if (!inPrivateorSpamChannel(message)) {
+    message.reply(wrongchannelmsg);
+    return;
+  }
+  fn.apply(null, [message, ...args]);
+}
+
+function doHelp(message, helpmsg) {
+  message.author.send(helpmsg);
+}
+
+function doBalance(message, tipper) {
+  aevo.getBalance(tipper, 1, function(err, balance) {
+    if (err) {
+      message.reply('Error getting Aevocoin (AEVO) balance.').then(message => message.delete(10000));
+    } else {
+    message.channel.send({ embed: {
+    description: '**:bank::money_with_wings::moneybag:Aevocoin (AEVO) Balance!:moneybag::money_with_wings::bank:**',
+    color: 1363892,
+    fields: [
+      {
+        name: '__User__',
+        value: '<@' + message.author.id + '>',
+        inline: false
+      },
+      {
+        name: '__Balance__',
+        value: '**' + balance.toString() + '**',
+        inline: false
+      }
+    ]
+  } });
+    }
+  });
+}
+
+function doDeposit(message, tipper) {
+  getAddress(tipper, function(err, address) {
+    if (err) {
+      message.reply('Error getting your Aevocoin (AEVO) deposit address.').then(message => message.delete(10000));
+    } else {
+    message.channel.send({ embed: {
+    description: '**:bank::card_index::moneybag:Aevocoin (AEVO) Address!:moneybag::card_index::bank:**',
+    color: 1363892,
+    fields: [
+      {
+        name: '__User__',
+        value: '<@' + message.author.id + '>',
+        inline: false
+      },
+      {
+        name: '__Address__',
+        value: '**' + address + '**',
+        inline: false
+      }
+    ]
+  } });
+    }
+  });
+}
+
+function doWithdraw(message, tipper, words, helpmsg) {
+  if (words.length < 4) {
+    doHelp(message, helpmsg);
+    return;
+  }
+
+  var address = words[2],
+    amount = getValidatedAmount(words[3]);
+
+  if (amount === null) {
+    message.reply("I don't know how to withdraw that much Aevocoin (AEVO)...").then(message => message.delete(10000));
+    return;
+  }
+
+  aevo.getBalance(tipper, 1, function(err, balance) {
+    if (err) {
+      message.reply('Error getting Aevocoin (AEVO) balance.').then(message => message.delete(10000));
+    } else {
+      if (Number(amount) + Number(paytxfee) > Number(balance)) {
+        message.channel.send('Please leave atleast ' + paytxfee + ' Aevocoin (AEVO) for transaction fees!');
+        return;
+      }
+      aevo.sendFrom(tipper, address, Number(amount), function(err, txId) {
+        if (err) {
+          message.reply(err.message).then(message => message.delete(10000));
+        } else {
+        message.channel.send({embed:{
+        description: '**:outbox_tray::money_with_wings::moneybag:Aevocoin (AEVO) Transaction Completed!:moneybag::money_with_wings::outbox_tray:**',
+        color: 1363892,
+        fields: [
+          {
+            name: '__Sender__',
+            value: '<@' + message.author.id + '>',
+            inline: true
+          },
+          {
+            name: '__Receiver__',
+            value: '**' + address + '**\n' + addyLink(address),
+            inline: true
+          },
+          {
+            name: '__txid__',
+            value: '**' + txId + '**\n' + txLink(txId),
+            inline: false
+          },
+          {
+            name: '__Amount__',
+            value: '**' + amount.toString() + '**',
+            inline: true
+          },
+          {
+            name: '__Fee__',
+            value: '**' + paytxfee.toString() + '**',
+            inline: true
+          }
+        ]
+      }});
+      }
+    });
+    }
+  });
+}
+
+function doTip(bot, message, tipper, words, helpmsg) {
+  if (words.length < 3 || !words) {
+    doHelp(message, helpmsg);
+    return;
+  }
+  var prv = false;
+  var amountOffset = 2;
+  if (words.length >= 4 && words[1] === 'private') {
+    prv = true;
+    amountOffset = 3;
+  }
+
+  let amount = getValidatedAmount(words[amountOffset]);
+
+  if (amount === null) {
+    message.reply("I don't know how to tip that much Aevocoin (AEVO)...").then(message => message.delete(10000));
+    return;
+  }
+
+  aevo.getBalance(tipper, 1, function(err, balance) {
+    if (err) {
+      message.reply('Error getting Aevocoin (AEVO) balance.').then(message => message.delete(10000));
+    } else {
+      if (Number(amount) + Number(paytxfee) > Number(balance)) {
+        message.channel.send('Please leave atleast ' + paytxfee + ' Aevocoin (AEVO) for transaction fees!');
+        return;
+      }
+
+      if (!message.mentions.users.first()){
+           message
+            .reply('Sorry, I could not find a user in your tip...')
+            .then(message => message.delete(10000));
+            return;
+          }
+      if (message.mentions.users.first().id) {
+        sendAEVO(bot, message, tipper, message.mentions.users.first().id.replace('!', ''), amount, prv);
+      } else {
+        message.reply('Sorry, I could not find a user in your tip...').then(message => message.delete(10000));
+      }
+    }
+  });
+}
+
+function sendAEVO(bot, message, tipper, recipient, amount, privacyFlag) {
+  getAddress(recipient.toString(), function(err, address) {
+    if (err) {
+      message.reply(err.message).then(message => message.delete(10000));
+    } else {
+          aevo.sendFrom(tipper, address, Number(amount), 1, null, null, function(err, txId) {
+              if (err) {
+                message.reply(err.message).then(message => message.delete(10000));
+              } else {
+                if (privacyFlag) {
+                  let userProfile = message.guild.members.find('id', recipient);
+                  userProfile.user.send({ embed: {
+                  description: '**:money_with_wings::moneybag:Aevocoin (AEVO) Transaction Completed!:moneybag::money_with_wings:**',
+                  color: 1363892,
+                  fields: [
+                    {
+                      name: '__Sender__',
+                      value: 'Private Tipper',
+                      inline: true
+                    },
+                    {
+                      name: '__Receiver__',
+                      value: '<@' + recipient + '>',
+                      inline: true
+                    },
+                    {
+                      name: '__txid__',
+                      value: '**' + txId + '**\n' + txLink(txId),
+                      inline: false
+                    },
+                    {
+                      name: '__Amount__',
+                      value: '**' + amount.toString() + '**',
+                      inline: true
+                    },
+                    {
+                      name: '__Fee__',
+                      value: '**' + paytxfee.toString() + '**',
+                      inline: true
+                    }
+                  ]
+                } });
+                message.author.send({ embed: {
+                description: '**:money_with_wings::moneybag:Aevocoin (AEVO) Transaction Completed!:moneybag::money_with_wings:**',
+                color: 1363892,
+                fields: [
+                  {
+                    name: '__Sender__',
+                    value: '<@' + message.author.id + '>',
+                    inline: true
+                  },
+                  {
+                    name: '__Receiver__',
+                    value: '<@' + recipient + '>',
+                    inline: true
+                  },
+                  {
+                    name: '__txid__',
+                    value: '**' + txId + '**\n' + txLink(txId),
+                    inline: false
+                  },
+                  {
+                    name: '__Amount__',
+                    value: '**' + amount.toString() + '**',
+                    inline: true
+                  },
+                  {
+                    name: '__Fee__',
+                    value: '**' + paytxfee.toString() + '**',
+                    inline: true
+                  }
+
+                ]
+              } });
+                  if (
+                    message.content.startsWith('!aevo private ')
+                  ) {
+                    message.delete(1000); //Supposed to delete message
+                  }
+                } else {
+                  message.channel.send({ embed: {
+                  description: '**:money_with_wings::moneybag:Aevocoin (AEVO) Transaction Completed!:moneybag::money_with_wings:**',
+                  color: 1363892,
+                  fields: [
+                    {
+                      name: '__Sender__',
+                      value: '<@' + message.author.id + '>',
+                      inline: true
+                    },
+                    {
+                      name: '__Receiver__',
+                      value: '<@' + recipient + '>',
+                      inline: true
+                    },
+                    {
+                      name: '__txid__',
+                      value: '**' + txId + '**\n' + txLink(txId),
+                      inline: false
+                    },
+                    {
+                      name: '__Amount__',
+                      value: '**' + amount.toString() + '**',
+                      inline: true
+                    },
+                    {
+                      name: '__Fee__',
+                      value: '**' + paytxfee.toString() + '**',
+                      inline: true
+                    }
+                  ]
+                } });
+                }
+              }
+            });
+    }
+  });
+}
+
+function getBlock(bot, msg){
+ getDataFromAPI("https://explorer.aevocoin.netv/api/getblockcount", true, function(result){
+   if (result !== "undefined") {
+      if (result) {
+        sendEmbedMessages(msg, "", 0x00AE86, [{
+          name: "AEVO current block",
+          value: "Block height: **" + result + "**"
+        }])
+      } else {
+        msg.reply("No block height found. Notify @FreakHouse of a problem, please.")
+      }
+   }
+ })
+}
+
+function getSupply(bot, msg){
+  getDataFromAPI("https://explorer.aevocoin.netv/ext/getmoneysupply", true, function(result){
+    if (result !== "undefined") {
+      if (result) {
+        sendEmbedMessages(msg, "", 0xF55555, [{
+          name: "AEVO current supply",
+          value: "Supply: **" + result + "**"
+        }])
+      } else {
+        msg.reply("No supply found. Notify @FreakHouse of a problem, please.")
+      }
+   }
+  })
+}
+
+function getDataFromAPI(url, sync, callback){
+  let xmlHttp = new XMLHttpRequest();
+  
+  xmlHttp.onreadystatechange = function() {
+    if (xmlHttp.readyState === 4 && xmlHttp.status === 200) {
+      callback(xmlHttp.responseText);
+    }
+  };
+  xmlHttp.open("GET", url, sync);
+  xmlHttp.send(null);
+}
+
+function sendEmbedMessages(msg, description, color, fields) {
+  let embed = new Discord.RichEmbed()
+    .setTitle("AevoCoin")
+    .setAuthor(msg.client.user.username, msg.client.user.avatarURL)
+    .setColor(color)
+    .setDescription(description)
+    .setFooter("© Aevo Coin", "https://explorer.aevocoin.net/images/logo.png")
+    .setTimestamp()
+    .setURL("https://explorer.aevocoin.net");
+  fields.forEach(function(f){
+    embed = embed.addField(f.name, f.value)
+  });
+  msg.channel.send(embed)
+}
+
+function getAddress(userId, cb) {
+  aevo.getAddressesByAccount(userId, function(err, addresses) {
+    if (err) {
+      cb(err);
+    } else if (addresses.length > 0) {
+      cb(null, addresses[0]);
+    } else {
+      aevo.getNewAddress(userId, function(err, address) {
+        if (err) {
+          cb(err);
+        } else {
+          cb(null, address);
+        }
+      });
+    }
+  });
+}
+
+function inPrivateorSpamChannel(msg) {
+  if (msg.channel.type == 'dm' || isSpam(msg)) {
+    return true;
+  } else {
+    return false;
+  }
+}
+
+function isSpam(msg) {
+  return spamchannels.includes(msg.channel.id);
+};
+
+
+function getValidatedAmount(amount) {
+  amount = amount.trim();
+  if (amount.toLowerCase().endsWith('aevo')) {
+    amount = amount.substring(0, amount.length - 3);
+  }
+  return amount.match(/^[0-9]+(\.[0-9]+)?$/) ? amount : null;
+}
+
+function txLink(txId) {
+  return 'https://explorer.aevocoin.net/tx/' + txId;
+}
+
+function addyLink(address) {
+  return 'https://explorer.aevocoin.net/address/' + address;
+}
