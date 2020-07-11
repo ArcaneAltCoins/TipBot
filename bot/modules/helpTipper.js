2e458b2e5c67eaed442d862e82c0f53fafa219ac
freakcoderz <42624259+freakcoderz@users.noreply.github.com>
2020-07-11 19:36:48 -0400

    Update helpTipper.js

--- bot/modules/helpTipper.js
+++ bot/modules/helpTipper.js
@@ -1,6 +1,7 @@
 'use strict';
 let config = require('config');
 let stoneFee = config.get('stone').paytxfee;
+let aevoFee = config.get('aevo').paytxfee;
 let prefix = config.get('bot').prefix;
 exports.commands = ['help'];
 exports.help = {
@@ -8,8 +9,9 @@ exports.help = {
   description: 'This commands has been changed to currency specific commands!',
   process: function(bot, message) {
     message.author.send(
-      '__**:bank: Coins :bank:**__\n' +
-      '  **Stonecoin (STONE) Tipper**\n    Transaction Fees: **' + stoneFee + '**\n' +    
+      '__**Supported Coins**__\n' +
+      '  **Stonecoin (STONE) Tipper**\n    Transaction Fees: **' + stoneFee + '**\n' +  
+      '  **Aevocoin (AEVO) Tipper**\n    Transaction Fees: **' + aevoFee + '**\n' + 
       '__**Commands**__\n' +
       '  **' + prefix + '<CoinSymbol>** : Displays This Message\n' +
       '  **' + prefix + '<CoinSymbol> balance** : get your balance\n' +
